914eba45c2667535975cbb136cc4bfe46c3c4d7c
bayuajinurmnsh <38206112+bayuajinurmnsh@users.noreply.github.com>
2021-02-12 21:42:57 +0700

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ MongoClient.connect(url, function(err, db) {
   
   dbo.collection("dataMakanan").find().toArray(function(err, result) { 
   if (err) throw err;
-  //console.log(result[1].makan_pagi+result[1].total);
+  //console.log(result[1].makan_pagi.menu1+result[1].total);
   //console.log(result);
 
 
@@ -70,7 +70,7 @@ router.post('/', express.json(), (req, res)=>{
     if(wc){
       a.add("Hai "+nama+", BMI Anda adalah "+bmi+"\n"+desc);
       a.add('Anda membutuhkan '+cal+' Kalori.');
-      a.add('hayBerikut Rekomendasi Makanan sesuai dengan Kebutuhan Kalori Anda');
+      a.add('Berikut Rekomendasi Makanan sesuai dengan Kebutuhan Kalori Anda');
       
      
 
@@ -92,7 +92,24 @@ router.post('/', express.json(), (req, res)=>{
       var agent_output;
         for (i = 0; i < result.length; i++) {
             if (result[i].total >= 1500 && result[i].total<1600) {
-              agent.add(result[i].makan_pagi+"\n kalori : "+result[i].total);
+              agent.add(
+                result[i].class+": \n Pagi (07:00): \n "+result[i].makan_pagi.menu1+" , takaran : "+result[i].makan_pagi.gram_menu1+"\n "+
+                result[i].makan_pagi.menu2+" , takaran : "+result[i].makan_pagi.gram_menu2+"\n Selingan (10:00) \n"+
+                result[i].selingan1.menu1+" , takaran : "+result[i].selingan1.gram_menu1+"\n "+ 
+                result[i].selingan1.menu2+" , takaran : "+result[i].selingan1.gram_menu2+"\n Siang (12:00) \n"+
+                result[i].makan_siang.menu1+" , takaran "+result[i].makan_siang.gram_menu1+"\n "+
+                result[i].makan_siang.menu2+" , takaran "+result[i].makan_siang.gram_menu2+"\n "+
+                result[i].makan_siang.menu3+" , takaran "+result[i].makan_siang.gram_menu3+"\n "+
+                result[i].makan_siang.menu4+" , takaran "+result[i].makan_siang.gram_menu4+"\n Selingan (15:00) \n"+
+                result[i].selingan2.menu1+" , takaran "+result[i].selingan2.gram_menu1+"\n "+
+                result[i].selingan2.menu2+" , takaran "+result[i].selingan2.gram_menu2+"\n Malam (19:00) \n "+
+                result[i].makan_malam.menu1+" , takaran "+result[i].makan_malam.gram_menu1+"\n "+
+                result[i].makan_malam.menu2+" , takaran "+result[i].makan_malam.gram_menu2+"\n "+
+                result[i].makan_malam.menu3+" , takaran "+result[i].makan_malam.gram_menu3+"\n "+
+                result[i].makan_malam.menu4+" , takaran "+result[i].makan_malam.gram_menu4+"\n Selingan (21:00) \n "+
+                result[i].selingan3.menu1+" , takaran "+result[i].selingan3.gram_menu1+"\n "
+            
+              );
             }
         }
     }
