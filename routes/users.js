0dbf6595daa54f32e3a58dc04d0a0e4ce6049786
bayuajinurmnsh <38206112+bayuajinurmnsh@users.noreply.github.com>
2021-02-12 10:23:41 +0700

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -83,6 +83,28 @@ router.post('/', express.json(), (req, res)=>{
     }
     else{
       agent.add("rekom makanana");
+      var MongoClient = require('mongodb').MongoClient;
+
+          // replace the uri string with your connection string.
+          var url = "mongodb+srv://dia_bot:1234@cluster0.qxkx4.mongodb.net/chtbot_diabot?retryWrites=true&w=majority";
+
+          MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("chtbot_diabot");
+             var query = { total: {$gte:1500 , $lt:1600} };
+                dbo.collection("dataMakanan").find(query).toArray(function(err, result) {
+                  if (err) throw err;
+                    var i;
+                    var hasil;
+                    
+                      //agent.add("PAGI (07.00) \n "+result[i].makan_pagi+"\n total : "+result[i].total);
+                      agent.add(result[1].makan_pagi);
+                      
+                    
+
+                    db.close();
+                  });
+            });
     }
     
 
