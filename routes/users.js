512d6331ccd2bd7c15869c70e6cd869e56b4e72c
bayuajinurmnsh <38206112+bayuajinurmnsh@users.noreply.github.com>
2021-02-13 23:35:51 +0700

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -89,15 +89,9 @@ router.post('/', express.json(), (req, res)=>{
 
     if (cal >= 1500 && cal <1600) {
       var i;
-      var gram;
-      var kalori_menu_tambahan;
-      var selisih_kalori;
-      
-
+      var agent_output;
         for (i = 0; i < result.length; i++) {
-            
-            
-            if (cal == result[i].total) {
+            if (result[i].total >= 1500 && result[i].total<1600) {
               agent.add(
                 result[i].class+": \nPagi (07:00): \n"+result[i].makan_pagi.menu1+" , takaran (gram) : "+result[i].makan_pagi.gram_menu1+"\n"+
                 result[i].makan_pagi.menu2+" , takaran (gram) : "+result[i].makan_pagi.gram_menu2+"\n\nSelingan (10:00) \n"+
@@ -117,40 +111,8 @@ router.post('/', express.json(), (req, res)=>{
                 "\nTotal kalori : "+result[i].total
             
               );
-            }//tutup if
-            else if(cal != result[i].total && cal <1600){
-            kalori_menu_tambahan = result[i].menu_tambahan.kalori_menu1;
-            selisih_kalori = cal - result[i].total;
-            gram = selisih_kalori /kalori_menu_tambahan;
-              
-              agent.add(
-                result[i].class+": \nPagi (07:00): \n"+result[i].makan_pagi.menu1+" , takaran (gram) : "+result[i].makan_pagi.gram_menu1+"\n"+
-                result[i].makan_pagi.menu2+" , takaran (gram) : "+result[i].makan_pagi.gram_menu2+"\n\nSelingan (10:00) \n"+
-                result[i].selingan1.menu1+" , takaran (gram) : "+result[i].selingan1.gram_menu1+"\n"+ 
-                result[i].selingan1.menu2+" , takaran (gram) : "+result[i].selingan1.gram_menu2+"\n\nSiang (12:00) \n"+
-                result[i].makan_siang.menu1+" , takaran (gram) : "+result[i].makan_siang.gram_menu1+"\n"+
-                result[i].makan_siang.menu2+" , takaran (gram) : "+result[i].makan_siang.gram_menu2+"\n"+
-                result[i].makan_siang.menu3+" , takaran (gram) : "+result[i].makan_siang.gram_menu3+"\n"+
-                result[i].makan_siang.menu4+" , takaran (gram) : "+result[i].makan_siang.gram_menu4+"\n\nSelingan (15:00) \n"+
-                result[i].selingan2.menu1+" , takaran (gram) : "+result[i].selingan2.gram_menu1+"\n"+
-                result[i].selingan2.menu2+" , takaran (gram) : "+result[i].selingan2.gram_menu2+"\n\nMalam (19:00) \n"+
-                result[i].makan_malam.menu1+" , takaran (gram) : "+result[i].makan_malam.gram_menu1+"\n"+
-                result[i].makan_malam.menu2+" , takaran (gram) : "+result[i].makan_malam.gram_menu2+"\n"+
-                result[i].makan_malam.menu3+" , takaran (gram) : "+result[i].makan_malam.gram_menu3+"\n"+
-                result[i].makan_malam.menu4+" , takaran (gram) : "+result[i].makan_malam.gram_menu4+"\n\nSelingan (21:00) \n"+
-                result[i].selingan3.menu1+" , takaran (gram) : "+result[i].selingan3.gram_menu1+"\n"+
-                result[i].menu_tambahan.menu1+" , takaran (gram) "+
-                "\nTotal kalori : "+cal
-            
-              );
-
-
-
-
-            }//tutup else if
-
-        }//ttup for
-
+            }
+        }
     }
     else if (cal >= 1000 && cal <1100) {
       var i;
